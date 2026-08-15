a12a437f6676fbd48d85813797e5c06ac4b443ee
Shivani Singh <singhshivani8306@gmail.com>
2026-08-15 21:24:28 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectToDatabase } from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import projectRouter from "./routes/projectRoutes.js";
+const app = express();
+
+await connectToDatabase()
+
+app.use(cors({
+    origin: /^http:\/\/localhost:\d+$/,
+    credentials: true
+}))
+app.use(cookieParser())
+app.use(express.json())
+
+app.get("/", (req, res)=> res.send("Server is Live!"))
+app.use('/api/auth', authRouter)
+app.use("/api/projects", projectRouter)
+
+// Centralized error handler 
+app.use((err, _req, res, _next)=>{
+    console.error(`[Error] ${err.message}`);
+    res.status(500).json({error: err.message})
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+})
